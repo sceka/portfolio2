c84bd509f3787e3283cc822de647f8360c8d5842
sceka <scekicm02@gmail.com>
2022-03-16 23:20:17 +0100

    Add blur and background image

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <div className="glass">Hello</div>
+    </div>
+  );
+};
+
+export default HomePage;
